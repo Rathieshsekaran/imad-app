24dc2646a35c3c5152dbe094e587a2b1f88a3110
Rathieshsekaran <30662753+Rathieshsekaran@users.noreply.github.com>
2017-08-09 00:48:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,70 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleone={
+    title:'Articleone |Rathieshsekaran',
+    heading:'article one',
+    date:'sept 15,1996',
+    content:`
+            <p>
+                
+              Robert Louis Stevenson spent his last five years in Samoa, where the locals couldn't comprehend how he earned his living as a writer... 
+                Robert Louis Stevenson spent his last five years in Samoa, where the locals couldn't comprehend how he earned his living as a writer... 
+            </p>
+            <p>
+                Robert Louis Stevenson spent his last five years in Samoa, where the locals couldn't comprehend how he earned his living as a writer... 
+                </p>`
+};
+function createtemplate(data){
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+var htmltemplate=`
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+                <div>
+            <a href="/">HOME</a>
+        </div>
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+            <p>
+                This the content of the third article
+            </p>
+            ${content}
+        </div>
+                </div>
+    </body>
+</html>
+`;
+return htmltemplate;
+}
+
+
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','article-one.html'));
+    res.send(createtemplate(articleone));
 });
 app.get('/article-two',function(req,res){
     res.sendFile(path.join(__dirname,'ui','article-two.html'));
