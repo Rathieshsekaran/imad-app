b3ac3f7ea9d75869e9950d62bce1efc130c9c369
Rathieshsekaran <30662753+Rathieshsekaran@users.noreply.github.com>
2017-08-09 02:39:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,9 @@ var img = document.getElementById('madi');
 var marginLeft= 0;
 img.onclick = function(){
     
-    setInterval( function() {
-            marginLeft += 10;
+    setInterval( function()
+    {
+            marginLeft += 5;
             img.style.marginLeft = marginLeft.toString() + "px";
-        } , 100);
+        } , 50);
 } ;
