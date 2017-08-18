f46133ea3c651bc1202c9db2ca2bea3a69722c16
Rathieshsekaran <30662753+Rathieshsekaran@users.noreply.github.com>
2017-08-18 15:17:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ return htmltemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var pool =new Pool(config);
+var pool =new pool(config);
 app.get('/test-db',function(req,res){
    pool.query('SELECT *FROM test',function(err,result){
        if(err){
