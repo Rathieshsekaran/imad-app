337eeea9cd802cb00f94d9375f64fda22c6deb75
Rathieshsekaran <30662753+Rathieshsekaran@users.noreply.github.com>
2017-08-18 15:58:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ app.get('/submit-name', function(req, res) {
 });
 
 
-app.get('/articles:articlename',function(req,res){
+app.get('/articles/:articlename',function(req,res){
 var articlename=req.params.articlename;
 pool.query("SELECT * FROM article WHERE title="+req.params.articleName+"'",function(err,result){
   if(err){
