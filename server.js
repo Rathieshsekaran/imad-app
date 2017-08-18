8fcc0f1b9e41218f9c2e58faf735d17771d5ea32
Rathieshsekaran <30662753+Rathieshsekaran@users.noreply.github.com>
2017-08-18 16:13:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,19 +114,15 @@ app.get('/submit-name', function(req, res) {
 });
 
 
-app.get('/article/:articleName',function(req,res){
+app.get('/articles/:articleName',function(req,res){
 pool.query("SELECT * FROM article WHERE title='"+req.params.articleName+"'",function(err,result){
   if(err){
       res.status(500).send(err.toString());
-  }  else {
-     if(result.rows.length===0){
-         res.status(404).send("articlenotfound");
-         
-     }else{
+  }  else{
          var articleData=result.rows[0];
              res.send(createTemplate(articleData));
      }
- }
+ 
 });
 
 });
