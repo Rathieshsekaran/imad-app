ce9f2afe26f9ba799d83619ebc40c39e23347d15
Rathieshsekaran <30662753+Rathieshsekaran@users.noreply.github.com>
2017-08-18 16:02:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,19 +114,18 @@ app.get('/submit-name', function(req, res) {
 });
 
 
-app.get('/articles/:articlename',function(req,res){
-var articlename=req.params.articlename;
-pool.query("SELECT * FROM article WHERE title="+req.params.articleName+"'",function(err,result){
+app.get('/articles/:articleName',function(req,res){
+pool.query("SELECT * FROM article WHERE title='"+req.params.articleName+"'",function(err,result){
   if(err){
       res.status(500).send(err.toString());
   }  else
  {
      if(result.rows.length===0){
-         res.status(400).send("articlenotfound");
+         res.status(404).send("articlenotfound");
          
      }else{
          var articleData=result.rows[0];
-             res.send(createtemplate(articles[articlename]));
+             res.send(createtemplate(articleData));
      }
  }
 });
